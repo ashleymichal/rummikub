c156dd5a7c7625de61b1cce2eaa2dcad7b7dd74b
Ashley Michal <ashleymichal@gmail.com>
2016-01-26 16:55:54 -0600

    Update run.rb

--- lib/run.rb
+++ lib/run.rb
@@ -1,4 +1,5 @@
 class Run
+  attr_accessor :tokens
   # Provide each instance of Run with a unique id.
   @@id = 0
 
@@ -15,23 +16,33 @@ class Run
     @tokens = tokens
   end
 
-  # tokens_to_add: array of token objects
-  # tokens_to_remote: array of token objects
-  def change(tokens_to_add, tokens_to_remove)
-    @tokens.concat(tokens_to_add)
+# separate out different behaviors of #change
+  def add_tokens(tokens)
+    tokens.concat(tokens)
+  end
+# also allowed for empty array rather than returning nil value
+  def remove_tokens(tokens)
     tokens_to_remove.each do |token|
-      @tokens.delete(token)
-    end
-
-    if @tokens.length > 0
-      # If the Run still has tokens associated with it return self.
-      return self
-    else
-      # If the run no longer has any tokens associated with it return nil.
-      # There is a better way than returning nil.
-      return nil
+      tokens.delete(token)
     end
   end
+  
+  # # tokens_to_add: array of token objects
+  # # tokens_to_remote: array of token objects
+  # def change(tokens_to_add, tokens_to_remove)
+  #   @tokens.concat(tokens_to_add)
+  #   tokens_to_remove.each do |token|
+  #     @tokens.delete(token)
+  #   end
+  #   if @tokens.length > 0
+  #     # If the Run still has tokens associated with it return self.
+  #     return self
+  #   else
+  #     # If the run no longer has any tokens associated with it return nil.
+  #     # There is a better way than returning nil.
+  #     return nil
+  #   end
+  # end
 
   def tokens
     return order_tokens

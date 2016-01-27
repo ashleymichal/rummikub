a0fd2e0dec1722faaa80731ef974c61f6a4c1035
David Mininger <dmininger+github@gmail.com>
2016-01-27 12:15:58 -0600

    Refactor Run and its associated tests.

--- lib/run.rb
+++ lib/run.rb
@@ -1,4 +1,6 @@
 class Run
+  attr_reader :id
+
   # Provide each instance of Run with a unique id.
   @@id = 0
 
@@ -12,35 +14,34 @@ class Run
 
   def initialize(tokens)
     @id = Run.send(:get_id)
-    @tokens = tokens
+    @unordered_tokens = tokens
   end
 
-  # tokens_to_add: array of token objects
-  # tokens_to_remote: array of token objects
-  def change(tokens_to_add, tokens_to_remove)
-    @tokens.concat(tokens_to_add)
+  # @param tokens_to_add [Array] token objects to be added to the run
+  def add(tokens_to_add)
+    unordered_tokens.concat(tokens_to_add)
+
+    tokens
+  end
+
+  # @param tokens_to_remove [Array] token objects to be removed from the run
+  def remove(tokens_to_remove)
     tokens_to_remove.each do |token|
-      @tokens.delete(token)
+      unordered_tokens.delete(token)
     end
 
-    if @tokens.length > 0
-      # If the Run still has tokens associated with it return self.
-      return self
-    else
-      # If the run no longer has any tokens associated with it return nil.
-      # There is a better way than returning nil.
-      return nil
-    end
+    tokens
   end
 
   def tokens
-    return order_tokens
+    order_tokens
   end
 
 private
+  attr_accessor :unordered_tokens
 
   def order_tokens
     # Fake order_tokens method.
-    return @tokens
+    return unordered_tokens
   end
 end
--- spec/run_spec.rb
+++ spec/run_spec.rb
@@ -1,26 +1,32 @@
 require_relative '../lib/run'
 
-RSpec.describe Run, "#tokens" do
+RSpec.describe Run, '#id' do
+  it "returns an auto-incrementing id" do
+    run1 = Run.new([:token])
+    run2 = Run.new([:token])
+    expect(run2.id).to eq (run1.id + 1)
+  end
+end
+
+RSpec.describe Run, '#tokens' do
   it "returns the instance's list of tokens" do
     run = Run.new([:RED1, :RED2, :RED3])
     expect(run.tokens).to eq [:RED1, :RED2, :RED3]
   end
 end
 
-RSpec.describe Run, "#change" do
-  context "with tokens to add" do
-    it "adds the tokens to the instance's list of tokens" do
-      run = Run.new([:RED1, :RED2, :RED3])
-      run.change([:RED4], [])
-      expect(run.tokens).to eq [:RED1, :RED2, :RED3, :RED4]
-    end
+RSpec.describe Run, '#add' do
+  it "adds given tokens to the instance's list of tokens" do
+    run = Run.new([:RED1, :RED2, :RED3])
+    expect(run.add([:RED4])).to eq [:RED1, :RED2, :RED3, :RED4]
+    expect(run.tokens).to eq [:RED1, :RED2, :RED3, :RED4]
   end
+end
 
-  context "with tokens to remove" do
-    it "removes the tokens from the instance's list of tokens" do
-      run = Run.new([:RED1, :RED2, :RED3, :RED4])
-      run.change([], [:RED4])
-      expect(run.tokens).to eq [:RED1, :RED2, :RED3]
-    end
+RSpec.describe Run, '#remove' do
+  it "removes given tokens from the instance's list of tokens" do
+    run = Run.new([:RED1, :RED2, :RED3, :RED4])
+    expect(run.remove([:RED4])).to eq [:RED1, :RED2, :RED3]
+    expect(run.tokens).to eq [:RED1, :RED2, :RED3]
   end
 end

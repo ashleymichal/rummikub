735ebfb90658486cd93b9ba5be6134d3c548f29a
Ashley Michal <ashleymichal@gmail.com>
2016-02-07 13:32:55 -0500

    Updates README for TokenGenerator

--- lib/token_generator.rb
+++ lib/token_generator.rb
@@ -1,6 +1,16 @@
 class TokenGenerator
-	def self.build_token color, number
-		token = Struct.new :color, :number
-		token.new(color, number)
-	end
+	@@colors = [:red, :blue, :yellow, :green]
+	@@numbers = (1..12)
+
+	private
+
+		def self.build_token color, number
+			token = Struct.new :color, :number
+			token.new(color, number)
+		end
+
+		def token_bag
+			## TODO: build bag with all permutations of @@colors & @@numbers x2
+		end
+		
 end
--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-Web based rummikub!
+## Rummikub
+
+#### Tokens
+
+At the start of each game, Rummikub generates a new Token Bag containing two each of all combinations of 4 colors and the numbers between 1-12.
+
+###### Example:
+
+token.color = :red
+
+token.number = 1
